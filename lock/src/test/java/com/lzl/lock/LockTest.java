d19ea25edeb9f0a38553ab042d38ccef992f5d49
liaozhilong <liaozhilong@daojia-inc.com>
2021-08-19 15:20:42 +0800

    父pom添加依赖管理等

---
+++ lock/src/test/java/com/lzl/lock/LockTest.java
@@ -0,0 +1,17 @@
+package com.lzl.lock;
+
+import org.junit.Test;
+
+/**
+ * @author liaozhilong
+ * @date 2021/8/19 14:49
+ * @Description
+ */
+
+public class LockTest {
+
+    @Test
+    public void test(){
+        System.out.printf("xixihaha");
+    }
+}
