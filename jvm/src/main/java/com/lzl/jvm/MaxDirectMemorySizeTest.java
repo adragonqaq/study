097ccf24f362380fd0899e1e6ee0fac60fcfe0a8
eren.liao <eren.liao@shijigroup.com>
2023-02-09 11:22:45 +0800

    -XX:MaxDirectMemorySize

---
+++ jvm/src/main/java/com/lzl/jvm/MaxDirectMemorySizeTest.java
@@ -0,0 +1,36 @@
+package com.lzl.jvm;
+
+/**
+ * -XX:MaxDirectMemorySize（重点）
+ * 内存不足时抛出OutOfMemoryError或OutOfMemoryError：Direct buffer memory。
+ * 如果使用Java自带的 ByteBuffer.allocateDirect(size) 或者直接 new DirectByteBuffer(capacity) , 这样受-XX:MaxDirectMemorySize 这个JVM参数的限制. 其实底层都是用的Unsafe#allocateMemory,区别是对大小做了限制. 如果超出限制直接OOM.
+ *
+ *
+ * 如果通过反射的方式拿到Unsafe的实例,然后用Unsafe的allocateMemory方法分配堆外内存. 确实不受-XX:MaxDirectMemorySize这个JVM参数的限制 . 所以限制的内存大小为操作系统的内存.
+ *
+ * 如果不设置-XX:MaxDirectMemorySize 默认的话,是跟堆内存大小保持一致. [堆内存大小如果不设置的话,默认为操作系统的 1/4, 所以 DirectMemory的大小限制JVM的Runtime.getRuntime().maxMemory()内存大小 . ]
+ *
+ * 看深入理解java虚拟机的"本机直接内存溢出"一节中，容易误认为-XX:MaxDirectMemorySize这个参数对Unsafe的allocateMemory方法也有效，事实上并不是，这个参数只对Java自带的 ByteBuffer.allocateDirect(size) 或者直接 new DirectByteBuffer(capacity) 才有效。
+ *
+ * Unsafe的allocateMemory方法限制的内存大小为操作系统的内存.
+ * ————————————————
+ *
+ * 原文链接：https://blog.csdn.net/qq_34626094/article/details/117173323
+ *
+ *
+ * @author eren.liao
+ * @date 2023/2/9 10:46
+ */
+public class MaxDirectMemorySizeTest {
+
+    public static void main(String[] args) {
+
+        // 得到的是byte
+        long l = Runtime.getRuntime().maxMemory();
+        //1G=1024MB，1MB=1024KB，1KB=1024   1G = 1073741824字节
+        // 1byte = 8bit
+        //“1byte等于8bit。数据存储是以“字节”（Byte）为单位，数据传输大多是以“位”（bit）为单位，一个位就代表一个0或1（即二进制），每8个位（bit）组成一个字节（Byte），是最小一级的信息单位
+        System.out.println(l);
+
+    }
+}
